3da02a92d3797a4973f8a3681e32e368d7f6d9cc
Manpreet Padam <manpreet.padam@kodefury.com>
2019-10-17 21:16:15 -0400

    Fixing date range issue

--- api_class.py
+++ api_class.py
@@ -15,7 +15,8 @@ class apiservice:
     twitter_api_id = None
     glassnode_api_url = None
     api_key=''
-    
+    from_date_unix=0
+    to_date_unix=0
 
     #calling the APIs
     def __init__(self):
@@ -23,15 +24,16 @@ class apiservice:
 
     def get_supply(self,symbols,duration=sd.Current,from_date='',to_date='',frequency_interval='24h'):
         supply_endpoint=f'{duration.name.lower()}'
-        self.glassnode_api_url+=f'supply/{supply_endpoint}?api_key={self.api_key}'
-
-        if from_date!='' and to_date !='':
-            from_date_unix=datetime.timestamp(pd.to_datetime(from_date))
-            to_date_unix=datetime.timestamp(pd.to_datetime(to_date))
-            self.glassnode_api_url+=f'&a={symbols}&s={from_date_unix}&u={to_date_unix}&i={frequency_interval}'
-        else:
-            self.glassnode_api_url+=f'&a={symbols}&i={frequency_interval}'
         
+        self.glassnode_api_url+=f'supply/{supply_endpoint}?api_key={self.api_key}&a={symbols}&i={frequency_interval}'
+        
+        if from_date != '' :
+            self.from_date_unix=int(datetime.timestamp(pd.to_datetime(from_date)))
+            self.glassnode_api_url += f'&s={self.from_date_unix}'
+        if to_date != '' :
+            self.to_date_unix=int(datetime.timestamp(pd.to_datetime(to_date)))
+            self.glassnode_api_url += f'&u={self.to_date_unix}'
+                
         supply_data=requests.get(self.glassnode_api_url).content
         df=pd.read_json(supply_data,orient='records')
         df.rename(columns={'t':'DateTime','v':'Amount'},inplace=True)
@@ -41,14 +43,14 @@ class apiservice:
 
     def get_Transactions(self,symbols,transactiontype=tt.Transfers_Volume_Sum,from_date='',to_date='',frequency_interval='24h'):
         transcation_endpoint=f'{transactiontype.name.lower()}'
-        self.glassnode_api_url+=f'transactions/{transcation_endpoint}?api_key={self.api_key}'
+        self.glassnode_api_url+=f'transactions/{transcation_endpoint}?api_key={self.api_key}&a={symbols}&i={frequency_interval}'
 
-        if from_date!='' and to_date !='':
-            from_date_unix=datetime.timestamp(pd.to_datetime(from_date))
-            to_date_unix=datetime.timestamp(pd.to_datetime(to_date))
-            self.glassnode_api_url+=f'&a={symbols}&s={from_date_unix}&u={to_date_unix}&i={frequency_interval}'
-        else:
-            self.glassnode_api_url+=f'&a={symbols}&i={frequency_interval}'
+        if from_date != '' :
+            self.from_date_unix=int(datetime.timestamp(pd.to_datetime(from_date)))
+            self.glassnode_api_url += f'&s={self.from_date_unix}'
+        if to_date != '' :
+            self.to_date_unix=int(datetime.timestamp(pd.to_datetime(to_date)))
+            self.glassnode_api_url += f'&u={self.to_date_unix}'
         
         transaction_data=requests.get(self.glassnode_api_url).content
         df=pd.read_json(transaction_data,orient='records')
@@ -59,14 +61,14 @@ class apiservice:
 
     def get_addresses(self,symbols,addresstype=adtype.Min_10k_Count,from_date='',to_date='',frequency_interval='24h'):
         address_endpoint=f'{addresstype.name.lower()}'
-        self.glassnode_api_url+=f'addresses/{address_endpoint}?api_key={self.api_key}'
+        self.glassnode_api_url+=f'addresses/{address_endpoint}?api_key={self.api_key}&a={symbols}&i={frequency_interval}'
 
-        if from_date!='' and to_date !='':
-            from_date_unix=datetime.timestamp(pd.to_datetime(from_date))
-            to_date_unix=datetime.timestamp(pd.to_datetime(to_date))
-            self.glassnode_api_url+=f'&a={symbols}&s={from_date_unix}&u={to_date_unix}&i={frequency_interval}'
-        else:
-            self.glassnode_api_url+=f'&a={symbols}&i={frequency_interval}'
+        if from_date != '' :
+            self.from_date_unix=int(datetime.timestamp(pd.to_datetime(from_date)))
+            self.glassnode_api_url += f'&s={self.from_date_unix}'
+        if to_date != '' :
+            self.to_date_unix=int(datetime.timestamp(pd.to_datetime(to_date)))
+            self.glassnode_api_url += f'&u={self.to_date_unix}'
         
         address_data=requests.get(self.glassnode_api_url).content
         df=pd.read_json(address_data,orient='records')
@@ -83,3 +85,5 @@ class apiservice:
         self.twitter_api_id = os.getenv('TWITTER_SECRET_KEY, TWITTER_KEY')
         self.api_key = os.getenv('GLASSNODE_KEY')
         self.glassnode_api_url='https://api.glassnode.com/v1/metrics/'
+        self.to_date_unix=0
+        self.from_date_unix=0
